1c98c31de7b8bfe576da84cc4e2855961547abe7
Dawood-Gondal <Dawood Gondal>
2025-07-25 12:13:05 +0500

    Updated Reg.php

--- registration.php
+++ registration.php
@@ -1,22 +1,9 @@
 <?php
 /**
- * MagePrince
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the mageprince.com license that is
- * available through the world-wide-web at this URL:
- * https://mageprince.com/end-user-license-agreement
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade this extension to newer
- * version in the future.
- *
- * @category    MagePrince
- * @package     Mageprince_Faq
- * @copyright   Copyright (c) MagePrince (https://mageprince.com/)
- * @license     https://mageprince.com/end-user-license-agreement
+ * @category    BugsBunny Enterprise
+ * @package     BugsBunny_OrderComment
+ * @copyright   Copyright (c) 2023 BugsBunny Enterprise
+ * @author      dawoodgondaldev@gmail.com
  */
 
 \Magento\Framework\Component\ComponentRegistrar::register(
